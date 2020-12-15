c3a9c59371befe94583d859aa85280c50412bde8
emineakbulut <ezz.akbulut@gmail.com>
2020-12-15 15:20:11 -0800

    Maps

---
+++ src/com/syntax/newClass33/DownloadFile1.java
@@ -0,0 +1,13 @@
+package com.syntax.newClass33;
+public class DownloadFile1 implements Runnable{
+    @Override
+    public void run() {
+        try {
+            System.out.println("starting the download...");
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Download successfully");
+    }
+}
